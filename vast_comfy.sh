065c9c4b0e09916564cbe4608b6be8dd0e18a2c4
BoldDog <olliver.fontana@gmail.com>
2026-05-10 14:28:48 +1000

    Removed mc_qwen.

--- vast_comfy.sh
+++ vast_comfy.sh
@@ -37,7 +37,6 @@ NODES=(
     "https://github.com/Acly/comfyui-inpaint-nodes"
     # "https://github.com/ltdrdata/ComfyUI-Impact-Pack"
     "https://github.com/jags111/efficiency-nodes-comfyui"
-    "https://github.com/tester4488/mc_qwen"
     "https://github.com/siraxe/ComfyUI-WanVideoWrapper_QQ"
     "https://github.com/chflame163/ComfyUI_LayerStyle"
     "https://github.com/chflame163/ComfyUI_LayerStyle_Advance"
